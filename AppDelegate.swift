51b75c2402759c84ac54410390729f9aad27db97
Felix Deimel <felix@lemonmojo.com>
2025-11-05 10:47:45 +0100

    More choices

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -3,43 +3,64 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet private var window: NSWindow!
-    @IBOutlet private weak var checkBoxMainQueue: NSButton!
+    @IBOutlet weak var popupButtonMode: NSPopUpButton!
     
-    private var invokeOnMainQueue: Bool {
-        checkBoxMainQueue.state == .on
+    enum InvocationMode {
+        case dispatchQueueMainAsync
+        case performSelectorOnMainThread
+        case functionCall
+    }
+    
+    private var invocationMode: InvocationMode {
+        switch popupButtonMode.indexOfSelectedItem {
+        case 0:
+            .dispatchQueueMainAsync
+        case 1:
+            .performSelectorOnMainThread
+        case 2:
+            .functionCall
+        default:
+            fatalError("Invalid Invocation Mode")
+        }
     }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        self.showAlert(onMainQueue: self.invokeOnMainQueue)
+        self.showAlert(invocationMode: self.invocationMode)
     }
     
     @IBAction private func showAlert_action(_ sender: Any) {
-        showAlert(onMainQueue: self.invokeOnMainQueue)
+        self.showAlert(invocationMode: self.invocationMode)
     }
     
-    private func showAlert(onMainQueue: Bool) {
-        if onMainQueue {
-            DispatchQueue.main.async(execute: show)
-        } else {
-            show()
+    private func showAlert(invocationMode: InvocationMode) {
+        switch invocationMode {
+        case .dispatchQueueMainAsync:
+            DispatchQueue.main.async(execute: _showAlert)
+        case .performSelectorOnMainThread:
+            performSelector(onMainThread: #selector(_showAlert),
+                            with: nil,
+                            waitUntilDone: false)
+        case .functionCall:
+            _showAlert()
         }
+    }
+    
+    @objc
+    func _showAlert() {
+        let alert = NSAlert()
         
-        func show() {
-            let alert = NSAlert()
-            
-            alert.messageText = "NSAlert with initialFirstResponder"
-            alert.informativeText = "The textfield should be focused."
-            
-            alert.addButton(withTitle: "OK")
-            
-            let textField = NSTextField(string: "")
-            textField.frame = .init(x: 0, y: 0, width: 300, height: 20)
-            textField.placeholderString = "Should be focused"
-            
-            alert.accessoryView = textField
-            alert.window.initialFirstResponder = textField
-            
-            alert.runModal()
-        }
+        alert.messageText = "NSAlert with initialFirstResponder"
+        alert.informativeText = "The textfield should be focused."
+        
+        alert.addButton(withTitle: "OK")
+        
+        let textField = NSTextField(string: "")
+        textField.frame = .init(x: 0, y: 0, width: 300, height: 20)
+        textField.placeholderString = "Should be focused"
+        
+        alert.accessoryView = textField
+        alert.window.initialFirstResponder = textField
+        
+        alert.runModal()
     }
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This demo project illustrates a bug in macOS Tahoe that prevents `NSWindow.initialFirstResponder` from taking effect when used via `NSAlert.runModal` and invoked through a `DispatchQueue.main.async` or `DispatchQueue.main.sync` call. 
 
-When running the demo app, unchecking the `Main Queue` checkbox makes the issue go away. The only difference then is that the `NSAlert.runModal` is not invoked through a `DispatchQueue.main` call.
+When running the demo app, switching the `Mode` to either `performSelectorOnMainThread` or `Function Call` makes the issue go away. So the issue only manifests when the `NSAlert` is called through `DispatchQueue.main`.
